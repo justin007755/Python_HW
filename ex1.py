18626093e45603915b3d085e0e203eb6af0ebf6f
justin007755 <justin007755@hotmail.com>
2017-07-20 17:13:45 +0800

    updated the exercises 1 again

--- ex1.py
+++ ex1.py
@@ -9,3 +9,12 @@ print "I'd much rather you 'not'."
 print 'I "said" do not touch this.'
 print "Nice try 'oh, yes'"
 print u'好消息'
+
+print "\n"
+
+# Make your script print only one of the lines.
+"""
+print "not this"
+print "not that"
+"""
+print "this"
